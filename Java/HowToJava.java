da761065fb0946e76cd9b9aef8c87a059eb20e78
Abhiek187 <achaudhuri2011@yahoo.com>
2020-08-20 18:15:36 -0400

    Add a static class variable

--- Java/HowToJava.java
+++ Java/HowToJava.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 enum State {
 	CALM,
 	ROWDY;
-	
+
 	@Override // not required, but helpful
 	public String toString() {
 		return name().toLowerCase();
@@ -21,19 +21,21 @@ abstract class SampleClass implements Class {
 	private String subject;
 	private int size;
 	protected State state;
-	
+	static int id = 0;
+
 	public SampleClass(String subject, int size) {
 		this.subject = subject;
 		this.size = size;
 		this.state = State.ROWDY;
+		this.id++;
 		System.out.println(this);
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Welcome to some class!";
+		return "Welcome to some class! You are student #" + id + ".";
 	}
-	
+
 	@Override
 	public void status() {
 		System.out.println("This " + this.subject + " class has " + this.size
@@ -46,10 +48,10 @@ class MathClass extends SampleClass {
 		super("Math", size);
 		this.state = State.ROWDY;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Welcome to math class!";
+		return "Welcome to math class! You are student #" + id + ".";
 	}
 }
 
@@ -58,10 +60,10 @@ class EnglishClass extends SampleClass {
 		super("English", size);
 		this.state = State.CALM;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Welcome to english class!";
+		return "Welcome to english class! You are student #" + id + ".";
 	}
 }
 
@@ -69,7 +71,7 @@ public class HowToJava {
 	public static void println(String str) {
 		System.out.println(str);
 	}
-	
+
 	public static int recursiveSum(int num) throws IllegalArgumentException {
 		if (num <= 0) {
 			throw new IllegalArgumentException("num must be positive");
@@ -79,24 +81,24 @@ public class HowToJava {
 			return num + recursiveSum(--num);
 		}
 	}
-	
+
 	// Generic Functions
 	public static <T> void printArr(T arr[]) {
 		for (T element : arr) {
 			System.out.print(element + " ");
 		}
-		
+
 		println("");
 	}
-	
+
 	public static <T> void printList(List<T> list) {
 		list.forEach(e -> System.out.print(e + " "));
 		println("size = " + list.size());
 	}
-	
+
 	/* This main function gets executed first
 	 * Since this class is public, it must match the file name
-	 * 
+	 *
 	 * public = can be called anywhere (not restricted by class or inherited classes)
 	 * static = can be called without an instance
 	 * (i.e. HowToJava.main instead of (new HowToJava()).main)
@@ -108,7 +110,7 @@ public class HowToJava {
 		System.out.println("Hello Java!");
 		System.out.println("By the way, you're using Java version "
 				+ System.getProperty("java.version"));
-		
+
 		// Variables
 		int myInt = -13;
 		double myDouble = 8.2945e1;
@@ -117,7 +119,7 @@ public class HowToJava {
 		String myString = "This is a reference type.";
 		final String nullVar = null; // final = constant
 		State state = State.CALM;
-		
+
 		// Input/Output
 		Scanner reader = new Scanner(System.in);
 		System.out.print("What's your name? ");
@@ -132,7 +134,7 @@ public class HowToJava {
 		System.out.println(); // prints just a newline
 		// Remember to close the scanner when you're done!
 		reader.close();
-		
+
 		// Math Operations
 		// +, -, *, /, and % behave like normal
 		System.out.println("Adding two numbers: " + myInt + " + " + myDouble + " = "
@@ -149,7 +151,7 @@ public class HowToJava {
 		formattedStr = String.format("Rounding: %f \u2192 %.2f", myDouble, myDouble);
 		System.out.println(formattedStr);
 		System.out.println();
-		
+
 		// Logical Operators
 		// &&, ||, and ! behave like normal
 		String hello1 = new String("hello");
@@ -158,10 +160,10 @@ public class HowToJava {
 				hello1 == hello2, hello1, hello2, hello1.equals(hello2));
 		System.out.println(formattedStr);
 		System.out.println();
-		
+
 		// Conditional Operators & Loops
 		System.out.println(nullVar == null ? "nullVar is null" : "nullVar isn't null");
-		
+
 		if (myInt % 3 == 0) {
 			System.out.println("myInt is divisible by 3");
 		} else if (myInt % 3 == 1) {
@@ -169,48 +171,48 @@ public class HowToJava {
 		} else {
 			System.out.println("off by 2");
 		}
-		
+
 		int num = 1;
 		do {
 			System.out.println(num);
 			num += 2;
 		} while (num < 10);
-		
+
 		for (num = 9; num > 0; num/=2) {
 			System.out.println(num);
 		}
-		
+
 		System.out.println();
-		
+
 		// Functions & Methods
 		println("An easier way to print");
 		println(myString + " is " + myString.length() + " characters long");
 		println("substring(1, 8): " + myString.substring(1, 8));
 		println("indexOf('s'): " + myString.indexOf('s'));
-		
+
 		int max = 10;
 		println("The sum from 0 to " + max + " = " + recursiveSum(max));
 		println(""); // but not perfect...
-		
+
 		// Arrays & Lists
 		if (args.length == 0) {
 			System.out.println("Hmm, you didn't enter any command line arguments.");
 		} else {
 			System.out.println("You passed in the following command line arguments:");
-			
+
 			int index = 0;
 			for (String arg : args) {
 				System.out.println("args[" + index + "] = " + arg);
 				index++;
 			}
 		}
-		
+
 		Integer[] nums1 = { 1, 2, 3, 4, 5 }; // must be Integer, not int, for generics
 		Integer[] nums2 = new Integer[5];
 		println("Fixed arrays:");
 		printArr(nums1);
 		printArr(nums2);
-		
+
 		List<Character> fakeString = new ArrayList<Character>();
 		fakeString.addAll(Arrays.asList('a', 't', 'r'));
 		fakeString.remove(0);
@@ -219,7 +221,7 @@ public class HowToJava {
 		printList(fakeString);
 		fakeString.clear();
 		println("");
-		
+
 		// Exception Handling
 		try {
 			recursiveSum(-1);
@@ -228,7 +230,7 @@ public class HowToJava {
 		} finally {
 			println("");
 		}
-		
+
 		// Classes
 		//SampleClass class0 = new SampleClass(); can't do this, SampleClass is abstract
 		MathClass class1 = new MathClass(50);
