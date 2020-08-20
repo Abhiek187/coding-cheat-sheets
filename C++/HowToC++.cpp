a31fe95d5b8d95f5d962b3b3cefcc6d9b150fe41
Abhiek187 <achaudhuri2011@yahoo.com>
2020-08-20 18:15:10 -0400

    Add more to C++
    took me 3 days to go through the entire learncpp site again, hope it's worth it!

--- C++/HowToC++.cpp
+++ C++/HowToC++.cpp
@@ -5,11 +5,14 @@
  *      Author: Abhiek187
  */
 
+#include "HowToC++.h"
+
+#include <array>
+#include <cmath>
+#include <cstddef>
 #include <iostream>
 #include <string>
-#include <cmath>
 #include <vector>
-#include "HowToC++.h"
 
 // using namespace std; // not good practice
 // <iostream>
@@ -19,72 +22,84 @@ using std::endl;
 using std::ostream;
 // <string>
 using std::string;
+using std::getline;
 // <cmath>
 using std::pow;
 using std::sqrt;
 using std::round;
+// <array>
+using std::array;
+// <cstddef>
+using std::size_t;
 // <vector>
 using std::vector;
 
 // Interface
-class Class {
+class IClass {
 public:
 	virtual void status() const = 0;
-	virtual ~Class() = default;
+	virtual ~IClass() = default;
 };
 
 // Abstract Class
-class SampleClass: public Class {
+class SampleClass: public IClass {
 protected:
 	string m_subject;
 	int m_size;
 	State m_state;
+	static int s_id;
 
 public:
 	SampleClass(const string &subject, int size, const State &state)
-		: m_subject(subject), m_size(size), m_state(state) {}
+		: m_subject(subject), m_size(size), m_state(state) {
+		s_id++;
+	}
 
-	~SampleClass() {
+	virtual ~SampleClass() {
 		cout << "You dropped out of " << m_subject << ".\n";
 	}
 
 	// toString(), can't have virtual friends :(
-	friend ostream& operator<<(ostream &out, const SampleClass &c) {
-		return c.print(out);
-	}
+	friend ostream& operator<<(ostream &out, const SampleClass &c);
 
 	virtual ostream& print(ostream &out) const {
-		out << "Welcome to some class!";
+		out << "Welcome to some class! You are student #" << s_id << '.';
 		return out;
 	}
 
-	virtual void status() const override {
-		string stateStr = this->m_state == 1 ? "rowdy" : "calm";
+	virtual void status() const override final {
+		string stateStr { this->m_state == State::rowdy ? "rowdy" : "calm" };
 		cout << "This " + this->m_subject << " class has " << this->m_size
 				<< " students and is " << stateStr << ".\n";
 	}
 };
 
-class MathClass: virtual public SampleClass {
+int SampleClass::s_id = 0;
+
+ostream& operator<<(ostream &out, const SampleClass &c) {
+	return c.print(out);
+}
+
+class MathClass final: virtual public SampleClass {
 public:
 	MathClass(int size): SampleClass{"Math", size, State::rowdy} {
 		cout << *this << '\n';
 	}
 
 	virtual ostream& print(ostream &out) const override {
-		out << "Welcome to math class!";
+		out << "Welcome to math class! You are student #" << s_id << '.';
 		return out;
 	}
 };
 
-class EnglishClass: virtual public SampleClass {
+class EnglishClass final: virtual public SampleClass {
 public:
 	EnglishClass(int size): SampleClass{"English", size, State::calm} {
 		cout << *this << '\n';
 	}
 
 	virtual ostream& print(ostream &out) const override {
-		out << "Welcome to english class!";
+		out << "Welcome to english class! You are student #" << s_id << '.';
 		return out;
 	}
 };
@@ -94,7 +109,7 @@ int add(int a, int b) {
 }
 
 void addPtrs(int *a, int *b, int &sum) {
-	sum = *a + *b;
+	sum = (!a && !b) ? 0 : *a + *b; // !a == nullptr
 }
 
 int recursiveSum(int num) {
@@ -108,46 +123,63 @@ int recursiveSum(int num) {
 }
 
 template <typename T>
-void printArr(T *arr, int length) {
+void printArr(const T *arr, int length) {
 	// Length is required since arrays decay into pointers
-	for (int i = 0; i < length; ++i) {
+	if (arr == nullptr) {
+		cout << "You didn't pass an array to printArr().\n";
+		return;
+	}
+
+	for (int i {0}; i < length; ++i) {
 		cout << arr[i] << ' ';
 	}
 
 	cout << endl;
 }
 
-template <typename T>
-void printVector(const vector<T> &v) {
-	for (const auto elem : v) {
+template <typename T, size_t size>
+void printStdArr(const array<T, size> &arr) {
+	for (const auto &elem : arr) {
 		cout << elem << ' ';
 	}
 
-	cout << "size = " << v.size() << '\n';
+	cout << "size = " << arr.size() << '\n';
+}
+
+template <typename T>
+void printVector(const vector<T> &v) {
+	for (const auto &elem : v) {
+		cout << elem << ' ';
+	}
+
+	cout << "size = " << v.size() << ", capacity = " << v.capacity() << '\n';
 }
 
 // main gets executed first
 int main(int argc, char *argv[])
 {
-	cout << "Hello C++!\n"; // endl flushes output every time, \n doesn't
+	cout << "Hello C++!\n"; // shorthand for std::cout
+	// endl flushes output every time, \n doesn't
 	cout << "By the way, you're using C++ version " << __cplusplus << endl;
 
 	// Variables
-	int myInt = -13;
-	double myDouble(8.2945e1);
-	char myChar {'\\'};
-	bool myBool = true;
-	string myString = "This is a string.";
-	State state = State::rowdy;
+	int myInt = -13; // copy initialization
+	double myDouble(8.2945e1); // direct initialization
+	char myChar = {'\\'}; // copy brace initialization
+	bool myBool {true}; // direct brace initialization (best way to initialize vars)
+	string myString {"This is a string."};
+	State state {State::rowdy};
+	Student student { "Joe Schmo" };
 
 	// Input/Output
 	cout << "What's your name? ";
-	string name;
+	string name {};
 	//cin >> name; // input for anything that's not a string
 	// cin.ignore(32767, '\n'); // then it's a good idea to ignore the rest
 	getline(cin, name);
 	cout << "Hey " << name << "!\n";
-	cout << "state: " << state << "\n\n";
+	cout << "state: " << static_cast<int>(state) << '\n';
+	cout << student.name << " got a " << student.grade << ".\n\n";
 
 	// Math Operations
 	cout << "Adding two numbers: " << myInt << " + " << myDouble << " = "
@@ -165,13 +197,19 @@ int main(int argc, char *argv[])
 	// &&, ||, and ! behave like normal
 
 	// Conditional Operators & Loops
-	// if, else if, else, switch, case, do, while, and for behave like normal
+	// if, else if, else, switch, case, do, and while behave like normal
 	cout << myInt << " is " << (!!myInt ? "truthy\n" : "falsy\n");
-	cout << 0 << " is " << (!!0 ? "truthy\n\n" : "falsy\n\n");
+	cout << 0 << " is " << (!!0 ? "truthy\n" : "falsy\n");
+	cout << "Multiple looping variables:\n";
+
+	for (int i {0}, j {5}; i < 5; ++i, --j) {
+		cout << "i = " << i << ", j = " << j << '\n';
+	}
+	cout << '\n';
 
 	// Pointers
-	int *myIntPtr = &myInt;
-	const int* null = nullptr;
+	int *myIntPtr {&myInt};
+	constexpr int *const null {nullptr}; // constant value and address
 	cout << "myInt is stored in address " << myIntPtr << '\n';
 	cout << "It references " << *myIntPtr << '\n';
 	*myIntPtr >>= 1; // 1101 (13) -> 0011 (-13) -> 0001 (-7) -> 0111 (7)
@@ -184,35 +222,37 @@ int main(int argc, char *argv[])
 	// Functions & Methods
 	cout << "Adding numbers: " << myInt << " + " << myInt << " = "
 			<< add(myInt, myInt) << '\n';
-	int sum = 0;
+	int sum {0};
 	addPtrs(myIntPtr, myIntPtr, sum);
 	cout << "sum reference resolves to " << sum << '\n';
 	cout << myString << " is " << myString.length() << " characters long\n";
 	cout << "substr(1, 8): " << myString.substr(1, 8) << '\n';
 	cout << "find('s'): " << myString.find('s') << '\n';
 
-	const int max {10};
+	constexpr int max {10};
 	cout << "The sum from 1 to " << max << " = " << recursiveSum(max) << "\n\n";
 
 	// Arrays & Vectors
-	if (argc == 1) {
+	if (argc <= 1) {
 		// argv[0] is the executable
 		cout << "Hmm, you didn't enter any command line arguments.\n";
 	} else {
 		cout << "You passed in the following arguments:\n";
 
-		for (int i = 0; i < argc; i++) {
+		for (int i {0}; i < argc; i++) {
 			cout << "argv[" << i << "] = " << argv[i] << '\n';
 		}
 	}
 
 	bool binary[] { true, false, false, false, true, false, true };
-	int binaryLength = sizeof(binary) / sizeof(*binary); // *(arr + i) == arr[i]
-	const int zerosLength = 5;
+	int binaryLength {sizeof(binary) / sizeof(*binary)}; // *(arr + i) == arr[i]
+	constexpr int zerosLength {5};
 	double zeros[zerosLength] {};
 	cout << "Fixed arrays:\n";
 	printArr(binary, binaryLength);
 	printArr(zeros, zerosLength);
+	array<string, 3> betterArr { "This", "is", "better." };
+	printStdArr(betterArr);
 
 	vector<char> fakeString { 'a', 't', 'r' };
 	fakeString.erase(fakeString.begin());
@@ -226,16 +266,15 @@ int main(int argc, char *argv[])
 	try {
 		recursiveSum(-1);
 	} catch (const char *err) {
-		cout << "Error: " << static_cast<string>(err) << "\n\n";
+		cout << "Error: " << err << "\n\n";
 	} catch (...) {
 		cout << "Wow you really broke the function!\n\n";
 	}
 
 	// Classes
-	// SampleClass is abstract
-	//SampleClass class0 = SampleClass("science", 20, STATE_ROWDY);
-	MathClass class1 = MathClass(50);
-	EnglishClass class2 = EnglishClass(30);
+	//SampleClass class0 {"science", 20, STATE_ROWDY}; // SampleClass is abstract
+	MathClass class1 {50};
+	EnglishClass class2 {30};
 	class1.status();
 	class2.status();
 
--- C++/HowToC++.h
+++ C++/HowToC++.h
@@ -8,21 +8,29 @@
 #ifndef HOWTOC___H_
 #define HOWTOC___H_
 
+#include <array>
+#include <string>
 #include <vector>
-using std::vector;
 
-enum State {
+enum class State {
 	calm, // 0
 	rowdy // 1
 };
 
+struct Student {
+	std::string name { "anonymous" };
+	int grade { 100 };
+};
+
 int add(int a, int b);
 void addPtrs(int *a, int *b, int *&sum);
-int recursiveSum(int num);
+int recursiveSum(int num=0);
 
 template <typename T>
-void printArr(T *arr, int length);
+void printArr(const T *arr, int length);
+template <typename T, size_t size>
+void printStdArr(const std::array<T, size> &arr);
 template <typename T>
-void printVector(const vector<T> &v);
+void printVector(const std::vector<T> &v);
 
 #endif /* HOWTOC___H_ */
