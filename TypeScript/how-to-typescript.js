5f315ea58389659074d319fa44c7a4e6b1557f0d
Abhiek187 <achaudhuri2011@yahoo.com>
2020-08-25 21:26:54 -0400

    Rearrange files

--- TypeScript/how-to-typescript.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Array push, pop, slice, map, filter
-console.log("Hello TypeScript!");
-//# sourceMappingURL=how-to-typescript.js.map
--- TypeScript/how-to-typescript.ts
+++ TypeScript/how-to-typescript.ts
@@ -1,2 +1,21 @@
+// forEach value and index
 // Array push, pop, slice, map, filter
+// Object key best practices, shorthand? (key = value)
+// Destructuring, spread & rest operators, arguments
+// Closure
 console.log("Hello TypeScript!");
+
+// Returns a function which returns a number
+const myCounter = (): () => number => {
+	let count = 0; // private variable
+
+	return () => {
+		return ++count;
+	};
+};
+
+const counter = myCounter();
+
+for (let i = 0; i < 5; i++) {
+	console.log(counter());
+}
