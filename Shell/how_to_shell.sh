d42e4aed013cb054748a40d35b33b50c6bbaa796
Abhiek187 <achaudhuri2011@yahoo.com>
2021-07-23 00:25:01 -0400

    Complete shell

--- Shell/how_to_shell.sh
+++ Shell/how_to_shell.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # ^ Shebang to run this script in a bash shell
 
+recursive_sum () {
+    # args = $1 $2 ...
+    if [ $# == 0 ]; then
+        echo "Error: num not provided"
+        return -1 # will underflow to 255
+    elif [ $1 -lt 0 ]; then
+        echo "Error: num must be positive"
+        return -1
+    elif [ $1 == 1 ]; then
+        return 1
+    else
+        recursive_sum $(( $1-1 ))
+        return $(( $1 + $? ))
+    fi
+}
+
 # No main function here!
 echo "Hello Shell!"
 echo "By the way, you're using this shell:"
@@ -19,30 +35,41 @@ unset NULL_VAR # remove the variable
 printf "What's your name? " # don't print a newline
 read NAME
 echo "Hey $NAME!"
-echo "NULL_VAR=$NULL_VAR"
+echo "NULL_VAR=${NULL_VAR:-"null"}" # :- == ?:
 echo "PID = $$"
 echo "" # print just a newline
 
 : "Math Operations
-Floats aren't support in ((expressions)), so need to use bc (bash calculator)"
+Floats aren't support in ((expressions)), so need to use bc (bash calculator)
+left | right == redirect output from left as input to right"
 echo "Adding two numbers: $MY_INT + $MY_DOUBLE = $(echo "$MY_INT + $MY_DOUBLE" | bc)"
 echo "Adding a number with a bool: $MY_INT + $MY_BOOL = $(($MY_INT + $MY_BOOL))"
-echo "Adding a number with a string: $MY_INT + $MY_STRING = $($MY_INT + $MY_STRING)"
 echo "Dividing two ints: $MY_INT / 2 = $(($MY_INT / 2))"
 echo "$MY_DOUBLE ^ $MY_INT = $(echo "$MY_DOUBLE ^ $MY_INT" | bc)"
 echo "Weird math: 0/0.0 = $(echo "0 / 0.0" | bc), sqrt(-1) = $(echo "sqrt(-1)" | bc)"
-echo -e "Rounding: $MY_DOUBLE \u2192 $(printf "%.2f" $MY_DOUBLE)" # -e to print unicode
+echo -e "Rounding: $MY_DOUBLE \u2192 $(printf "%.2f" $MY_DOUBLE)" # -e to enable escaped characters
 echo ""
 
 # Logical Operators
+# -eq: ==, -ne: !=, -gt: >, -lt: <, -ge: >=, -le: <=
 [[ ! $MY_BOOL || $MY_INT -le 0 ]] && echo true || echo false # -le == <=
 
 # Conditional Operators & Loops
-# if, else if, else, switch, case, do, while, and for behave like normal?
 echo "File: $0"
+
+case $0 in
+    "./how_to_shell.sh") echo "This script is executed in a separate shell."
+    ;; # break
+    "-bash"|"/bin/bash") echo "This script is executed in the same shell."
+    ;;
+    *) echo "You executed this file in a unique way." # default
+esac
 
 if [ $# == 0 ]; then
     echo "Hmm you didn't enter any command line arguments."
+elif [ $# -lt 0 ]
+then
+    echo "That's not possible."
 else
     echo "$# arguments:"
     I=1
@@ -55,13 +82,38 @@ else
     done
 fi
 
+COUNT=5
+# Or while [ COUNT -ge 0 ]
+until [ $COUNT -lt 0 ]
+do
+    echo $COUNT
+    ((COUNT--))
+
+    if [ $COUNT == 0 ]; then
+        echo "Blast off!"
+        break
+    fi
+done
+
+echo ""
+
 # Functions & Methods
+STR_LENGTH=`echo $MY_STRING | wc -m` # `evaluate expression as a string`
+echo "$MY_STRING is $STR_LENGTH characters long."
+echo "substring(1, 8): ${MY_STRING:1:8}" # {string:start:length}
+STR_INDEX_OF=`echo $MY_STRING | grep -bo 's'`
+echo "indexOf('s'): $STR_INDEX_OF"
+
+recursive_sum
+echo "recursive_sum() = $?" # $? = return value
+recursive_sum -1
+echo "recursive_sum(-1) = $?"
+recursive_sum 10
+echo "recursive_sum(10) = $?"
+unset -f recursive_sum # remove the definition of a function
+echo ""
 
 # Arrays
 MY_ARRAY=(3 -4 8 -5 0 9 -1)
 MY_ARRAY[2]="cheese"
 echo "MY_ARRAY = ${MY_ARRAY[*]}" # see comment above about * vs @
-
-# Exception Handling
-
-# Classes
