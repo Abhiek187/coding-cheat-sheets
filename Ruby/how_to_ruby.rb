fd186ae4fa442c2c441b770db773fcc74c6e1b21
Abhiek187 <achaudhuri2011@yahoo.com>
2021-11-05 21:39:15 -0400

    Make program more robust and convert variables to snake case

--- Ruby/how_to_ruby.rb
+++ Ruby/how_to_ruby.rb
@@ -1,11 +1,11 @@
 require "cmath"
 
-def sumTo(n=1)
+def sum_to(n=1)
 	if n < 1
 		raise ArgumentError.new("n must be positive")
 	end
 
-	if n == 1 then n else n + sumTo(n - 1) end
+	if n == 1 then n else n + sum_to(n - 1) end
 end
 
 def generic_func(*args)
@@ -60,47 +60,56 @@ puts "Hello Ruby!" # automatic newline
 print "By the way, you're running Ruby #{RUBY_VERSION}\n" # no newline
 
 # Variables
-myInt = -5
-myDouble = 8.3945792e1
-myBool = true
-myString = "This is a string."
+my_int = -5
+my_double = 8.3945792e1
+my_bool = true
+my_string = "This is a string."
 NULL = nil # constant
 
 # Input/Output
 print "Enter your name: "
-name = gets.chomp
-name.capitalize!
+# gets by default will try to read from a file and interfere with ARGV
+name = (STDIN.gets || "").chomp
+name.capitalize! # ! == in-place
 puts "Hi #{name}!"
 puts "null: #{NULL}"
 puts "\n" # prints just a newline
 
 # Math Operations
-puts "#{myInt} + #{myDouble} = #{myInt + myDouble}"
-puts "Complex math: #{myInt} ^ #{myDouble} = #{myInt ** myDouble}"
-puts "#{myInt} mod #{myDouble} = #{myInt % myDouble}"
+puts "#{my_int} + #{my_double} = #{my_int + my_double}"
+puts "Complex math: #{my_int} ^ #{my_double} = #{my_int ** my_double}"
+puts "#{my_int} mod #{my_double} = #{my_int % my_double}"
 puts "Weird math: 0/0.0 = #{0/0.0}, sqrt(-1) = #{CMath.sqrt(-1)}"
-puts "Rounding: #{myDouble} \u2192 #{myDouble.round(2)}"
+puts "Rounding: #{my_double} \u2192 #{my_double.round(2)}"
 puts "\n"
 
 # Logical Operators
 # &&, ||, and ! behave like normal
 
 # Conditional Operators & Loops
-if myDouble < 0
-	puts "myDouble is negative"
-elsif myDouble > 0
-	puts "myDouble is positive"
+if my_double < 0
+	puts "my_double is negative"
+elsif my_double > 0
+	puts "my_double is positive"
 else
-	puts "myDouble is 0"
+	puts "my_double is 0"
 end
 
-unless myDouble.to_i % 2 == 0 # unless == if !...
-	puts "myDouble as an int (#{myDouble.to_i}) is odd"
+unless my_double.to_i % 2 == 0 # unless == if !...
+	puts "my_double as an int (#{my_double.to_i}) is odd"
 end
 
 for i in 1...10 # one less dot includes 10
 	next if i % 3 == 0
 	puts i
+end
+
+if ARGV.length > 0
+	for i in 0...ARGV.length
+		puts "ARGV[#{i}] = #{ARGV[i]}"
+	end
+else
+	puts "Hmm, you didn't enter any command line arguments."
 end
 
 i = 5
@@ -122,32 +131,32 @@ puts "Ruby\n\n"
 
 # Functions & Methods
 generic_func("lucky", 7, true)
-puts "sum(1, 10) = #{sumTo(10)}"
-puts "#{myString} is #{myString.length} characters long"
-puts "#{myString} backwards is #{myString.reverse}"
-puts "Shouting #{myString} yields #{myString.upcase}"
-puts "myString[#{myInt}..-2] = #{myString[myInt..-2]}"
-puts "s is located at index #{myString.index('s')}"
+puts "sum(1, 10) = #{sum_to(10)}"
+puts "#{my_string} is #{my_string.length} characters long"
+puts "#{my_string} backwards is #{my_string.reverse}"
+puts "Shouting #{my_string} yields #{my_string.upcase}"
+puts "my_string[#{my_int}..-2] = #{my_string[my_int..-2]}"
+puts "s is located at index #{my_string.index('s')}"
 puts "\n"
 
 # Arrays & Hashes
-myArray = [0, "is", false]
-myArray << []
-myArray.each { |elem| puts "#{elem}" }
+my_array = [0, "is", false]
+my_array << []
+my_array.each { |elem| puts "#{elem}" }
 
-myHash = Hash.new # empty hash
-myHash = { # hash with symbolic keys
+my_hash = Hash.new # empty hash
+my_hash = { # hash with symbolic keys
 	:id => 1000101,
 	name: "Joe"
 }
 
-myHash.each do |key, value|
+my_hash.each do |key, value|
 	puts "#{key} = #{value}"
 end
 
 # Exception Handling
 begin
-	sumTo(-1)
+	sum_to(-1)
 rescue ArgumentError => msg
 	puts "Error: #{msg}"
 ensure
@@ -155,7 +164,7 @@ ensure
 end
 
 # Classes
-mathClass = MathClass.new(50)
-englishClass = EnglishClass.new(30)
-mathClass.status
-englishClass.status
+math_class = MathClass.new(50)
+english_class = EnglishClass.new(30)
+math_class.status
+english_class.status
