3c3271fab20bc073fa0ec44ff13c69f8d171061c
Abhiek187 <achaudhuri2011@yahoo.com>
2022-01-23 20:16:52 -0500

    Use single quotes instead of double quotes for strings

--- SQL/HowToSQL.sql
+++ SQL/HowToSQL.sql
@@ -19,134 +19,134 @@ CREATE TABLE IF NOT EXISTS Reviews (
 );
 
 INSERT INTO Games (Name, Ratings)
-    VALUES ("Phantasy Star Online 2", 939);
+    VALUES ('Phantasy Star Online 2', 939);
 INSERT INTO Games (Name, Ratings, Price)
-    VALUES ("Minecraft", 57636, 29.99);
+    VALUES ('Minecraft', 57636, 29.99);
 INSERT INTO Games (Name, Ratings, Price)
-    VALUES ("ROBLOX", 1939, 0);
+    VALUES ('ROBLOX', 1939, 0);
 INSERT INTO Games (Name, Ratings, Price)
-    VALUES ("Microsoft Flight Simulator", 51, 119.99);
+    VALUES ('Microsoft Flight Simulator', 51, 119.99);
 INSERT INTO Games (Name, Ratings, Price)
-    VALUES ("Candy Crush Soda Saga", 1996, 0);
+    VALUES ('Candy Crush Soda Saga', 1996, 0);
 INSERT INTO Games (Name, Ratings, Price)
-    VALUES ("Caesars Casino", 1837, 0);
+    VALUES ('Caesars Casino', 1837, 0);
 
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Phantasy Star Online 2"),
-        "2020-07-08",
-        "garbage store",
+        (SELECT GameID FROM Games WHERE Name = 'Phantasy Star Online 2'),
+        '2020-07-08',
+        'garbage store',
         1,
-        "Microsoft store doesn't work for ****"
+        'Microsoft store doesn''t work for ****'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Phantasy Star Online 2"),
-        "2020-08-03",
-        "Store issue",
+        (SELECT GameID FROM Games WHERE Name = 'Phantasy Star Online 2'),
+        '2020-08-03',
+        'Store issue',
         5,
-        "The game itself is amazing. It's just the Microsoft Store breaks the game sometimes"
+        'The game itself is amazing. It''s just the Microsoft Store breaks the game sometimes'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Phantasy Star Online 2"),
-        "2020-07-24",
-        "garbo",
+        (SELECT GameID FROM Games WHERE Name = 'Phantasy Star Online 2'),
+        '2020-07-24',
+        'garbo',
         1,
-        "Deserves nothing more than one star if you give it more you are not very smart. That's why games don't get fixed. Worst launch in history lol. if I restart my pc it uninstalls the game Update: Still broken unfortunately, talked to Microsoft in a call and chat, even screen shared to get them to uninstall the game, after they failed (which led to another of many restores) they blamed sega for the issues, don't spend money like I did spend quite a bit and also just got prem because seemed like the game was fine then it broke again of course and now cant seem to play at all. ac is non refundable so ya know."
+        'Deserves nothing more than one star if you give it more you are not very smart. That''s why games don''t get fixed. Worst launch in history lol. if I restart my pc it uninstalls the game Update: Still broken unfortunately, talked to Microsoft in a call and chat, even screen shared to get them to uninstall the game, after they failed (which led to another of many restores) they blamed sega for the issues, don''t spend money like I did spend quite a bit and also just got prem because seemed like the game was fine then it broke again of course and now cant seem to play at all. ac is non refundable so ya know.'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Minecraft"),
-        "2020-08-14",
-        "our family favorite",
+        (SELECT GameID FROM Games WHERE Name = 'Minecraft'),
+        '2020-08-14',
+        'our family favorite',
         5,
-        "this game is awesome because I can play, and my 7 & 6 year old can play...and we all have fun!! They can play together or by themselves...but the best is that we can all 3 play together. It makes for hours of fun. The cross platform is great. I play on a laptop, 1 kid on a switch, the other kid on a tablet or ps4. It's the best hours of pass time during quarantine for us. Especially because the game doesn't get boring...you can build endlessly, or work for it in survival. This game actually even taught my 7 year old to read and write lol. From wanting to chat so bad, she eventually learned how to read it and type it. It boosts their creativity, and my youngest has gained confidence of being able to learn and teach mommy some things. I have not one bad thing to say!!"
+        'this game is awesome because I can play, and my 7 & 6 year old can play...and we all have fun!! They can play together or by themselves...but the best is that we can all 3 play together. It makes for hours of fun. The cross platform is great. I play on a laptop, 1 kid on a switch, the other kid on a tablet or ps4. It''s the best hours of pass time during quarantine for us. Especially because the game doesn''t get boring...you can build endlessly, or work for it in survival. This game actually even taught my 7 year old to read and write lol. From wanting to chat so bad, she eventually learned how to read it and type it. It boosts their creativity, and my youngest has gained confidence of being able to learn and teach mommy some things. I have not one bad thing to say!!'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Minecraft"),
-        "2020-08-12",
-        "Minecraft a holy experience.",
+        (SELECT GameID FROM Games WHERE Name = 'Minecraft'),
+        '2020-08-12',
+        'Minecraft a holy experience.',
         5,
-        "This game is the pure embodiment of heavenly light and holy entertainment. It must have be forged in the heart of a dying star. This game is the purest form of gaming to ever exist. I have been blessed to have received this game as a gift. Bless Mojang, Microsoft, Notch and every one who has ever worked on this game. I am honored to have played this game as it has changed my life for good. Thank you, and god bless you all."
+        'This game is the pure embodiment of heavenly light and holy entertainment. It must have be forged in the heart of a dying star. This game is the purest form of gaming to ever exist. I have been blessed to have received this game as a gift. Bless Mojang, Microsoft, Notch and every one who has ever worked on this game. I am honored to have played this game as it has changed my life for good. Thank you, and god bless you all.'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Minecraft"),
-        "2020-08-13",
-        "Amazing",
+        (SELECT GameID FROM Games WHERE Name = 'Minecraft'),
+        '2020-08-13',
+        'Amazing',
         5,
-        "its better then fortnite"
+        'its better then fortnite'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "ROBLOX"),
-        "2020-06-11",
-        "VERY GOOD BUT SOME SMALL PROBLEMS",
+        (SELECT GameID FROM Games WHERE Name = 'ROBLOX'),
+        '2020-06-11',
+        'VERY GOOD BUT SOME SMALL PROBLEMS',
         5,
-        "BEST GAME EVER I PLAY DID 3.5 HOURS EVERYDAY except I HATE the chat area you can't even type NUMBERS??!?!?!?!?!?!!"
+        'BEST GAME EVER I PLAY DID 3.5 HOURS EVERYDAY except I HATE the chat area you can''t even type NUMBERS??!?!?!?!?!?!!'
     );
 INSERT INTO Reviews (GameID, Day, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "ROBLOX"),
-        "2020-06-10",
+        (SELECT GameID FROM Games WHERE Name = 'ROBLOX'),
+        '2020-06-10',
         5,
-        "THERE IS NEVER BEST GAME AND PRO GAME!!!!!!"
+        'THERE IS NEVER BEST GAME AND PRO GAME!!!!!!'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "ROBLOX"),
-        "2020-08-09",
-        "AMAZING APP",
+        (SELECT GameID FROM Games WHERE Name = 'ROBLOX'),
+        '2020-08-09',
+        'AMAZING APP',
         5,
-        "I really like this game. It kills time and is really fun."
+        'I really like this game. It kills time and is really fun.'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Candy Crush Soda Saga"),
-        "2020-08-06",
-        "Wonderful",
+        (SELECT GameID FROM Games WHERE Name = 'Candy Crush Soda Saga'),
+        '2020-08-06',
+        'Wonderful',
         5,
-        "Love it. Being home bound I play a lot."
+        'Love it. Being home bound I play a lot.'
     );
 INSERT INTO Reviews (GameID, Day, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Candy Crush Soda Saga"),
-        "2020-07-30",
+        (SELECT GameID FROM Games WHERE Name = 'Candy Crush Soda Saga'),
+        '2020-07-30',
         5,
-        "fun"
+        'fun'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Candy Crush Soda Saga"),
-        "2020-08-14",
-        "Great Fun kinda got harder",
+        (SELECT GameID FROM Games WHERE Name = 'Candy Crush Soda Saga'),
+        '2020-08-14',
+        'Great Fun kinda got harder',
         5,
-        "Running out of turns lol"
+        'Running out of turns lol'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Caesars Casino"),
-        "2019-06-18",
-        "GOOD TIMES",
+        (SELECT GameID FROM Games WHERE Name = 'Caesars Casino'),
+        '2019-06-18',
+        'GOOD TIMES',
         5,
-        "FUN FUN FUN"
+        'FUN FUN FUN'
     );
 INSERT INTO Reviews (GameID, Day, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Caesars Casino"),
-        "2019-06-05",
+        (SELECT GameID FROM Games WHERE Name = 'Caesars Casino'),
+        '2019-06-05',
         5,
-        "Great graphics"
+        'Great graphics'
     );
 INSERT INTO Reviews (GameID, Day, Title, Stars, Review)
     VALUES (
-        (SELECT GameID FROM Games WHERE Name = "Caesars Casino"),
-        "2019-05-19",
-        "Can't win if you don't play!",
+        (SELECT GameID FROM Games WHERE Name = 'Caesars Casino'),
+        '2019-05-19',
+        'Can''t win if you don''t play!',
         5,
-        "The different slots or great!"
+        'The different slots or great!'
     );
 
 -- Read
@@ -154,19 +154,19 @@ SELECT * FROM Games;
 SELECT * FROM Reviews
     LIMIT 5;
 
-SELECT Name, Ratings AS "Reviews"
+SELECT Name, Ratings AS 'Reviews'
     FROM Games
     WHERE Ratings BETWEEN 1000 AND 3000
     OR Price = 0
     ORDER BY Ratings DESC;
 SELECT GameID, Stars, Review
     FROM Reviews
-    WHERE Review LIKE "%great%";
-SELECT GameID, COUNT(*) AS "TotalReviews"
+    WHERE Review LIKE '%great%';
+SELECT GameID, COUNT(*) AS 'TotalReviews'
     FROM Reviews
     GROUP BY GameID
     HAVING COUNT(*) > 0;
-SELECT GameID, AVG(stars) AS "AverageRating"
+SELECT GameID, AVG(stars) AS 'AverageRating'
     FROM Reviews
     GROUP BY GameID;
 
@@ -179,7 +179,7 @@ SELECT Games.Name, Games.Price, Reviews.Day, Reviews.Title, Reviews.Stars
 -- Update
 UPDATE Reviews
     SET Stars = 3
-    WHERE Title = "Great Fun kinda got harder";
+    WHERE Title = 'Great Fun kinda got harder';
 
 -- Delete
 DELETE FROM Reviews
